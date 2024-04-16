aacc1dcaeb6ec04e64db94c8ff96b09a712080e7
Matt Stucky <59749326+mnstucky@users.noreply.github.com>
2024-04-16 16:55:09 +0000

    Try to read stuff

--- src/Server.cs
+++ src/Server.cs
@@ -9,5 +9,14 @@ TcpListener server = new(IPAddress.Any, 4221);
 server.Start();
 var client = server.AcceptTcpClient();
 var stream = client.GetStream();
+var request = "";
+var requestBuffer = new byte[256];
+var requestPosition = 0;
+while (stream.Read(requestBuffer, requestPosition, 256) > 0)
+{
+    request += System.Text.Encoding.Default.GetString(requestBuffer);
+    requestPosition += 256;
+}
+Console.WriteLine(request);
 var message = System.Text.Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
 stream.Write(message, 0, message.Length);
