4e9bdb7915bf0f7360924af93d798d0fc0f51645
Matt Stucky <59749326+mnstucky@users.noreply.github.com>
2024-04-17 02:44:49 +0000

    Remove debug message

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
-// Uncomment this block to pass the first stage
 TcpListener server = new(IPAddress.Any, 4221);
 server.Start();
 var client = server.AcceptTcpClient();
@@ -15,6 +11,5 @@ while (stream.Read(requestBuffer, 0, 256) > 0)
 {
     request += System.Text.Encoding.Default.GetString(requestBuffer);
 }
-Console.WriteLine(request);
 var message = System.Text.Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
 stream.Write(message, 0, message.Length);
