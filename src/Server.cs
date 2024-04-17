f7fbe37e8cfb2f1b36374de190357081eafc76f0
Matt Stucky <59749326+mnstucky@users.noreply.github.com>
2024-04-17 03:11:10 +0000

    Refactor

--- src/Server.cs
+++ src/Server.cs
@@ -2,14 +2,40 @@ using System.Net;
 using System.Net.Sockets;
 
 TcpListener server = new(IPAddress.Any, 4221);
-server.Start();
-var client = server.AcceptTcpClient();
-var stream = client.GetStream();
-var request = "";
-var requestBuffer = new byte[256];
-while (stream.Read(requestBuffer, 0, 256) > 0)
+
+try
 {
-    request += System.Text.Encoding.Default.GetString(requestBuffer);
+    server.Start();
+    while (true)
+    {
+        using var client = await server.AcceptTcpClientAsync();
+        using var stream = client.GetStream();
+        var requestBuffer = new byte[1024];
+        await stream.ReadAsync(requestBuffer);
+        var request = System.Text.Encoding.UTF8.GetString(requestBuffer);
+        var requestLine = request.Split("\r\n").FirstOrDefault();
+        var requestLineParts = requestLine?.Split(" ");
+        var httpMethod = requestLineParts?.FirstOrDefault();
+        var requestTarget = requestLineParts?.Skip(1).FirstOrDefault();
+
+        if (string.IsNullOrWhiteSpace(requestLine) || string.IsNullOrWhiteSpace(httpMethod)
+            || string.IsNullOrWhiteSpace(requestTarget))
+        {
+            var badRequest = System.Text.Encoding.ASCII.GetBytes("HTTP/1.1 400 Bad Request\r\n\r\n");
+            stream.Write(badRequest, 0, badRequest.Length);
+            continue;
+        }
+
+        var message = requestTarget switch
+        {
+            "/" => "HTTP/1.1 200 OK\r\n\r\n",
+            _ => "HTTP/1.1 404 Not Found\r\n\r\n"
+        };
+        var messageBytes = System.Text.Encoding.ASCII.GetBytes(message);
+        stream.Write(messageBytes, 0, messageBytes.Length);
+    }
 }
-var message = System.Text.Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
-stream.Write(message, 0, message.Length);
+finally
+{
+    server.Dispose();
+}
