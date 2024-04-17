7a2900ac769046dd2a01002a96ec82a47e3a113e
Matt Stucky <59749326+mnstucky@users.noreply.github.com>
2024-04-17 03:41:21 +0000

    Add echo

--- src/Server.cs
+++ src/Server.cs
@@ -16,19 +16,25 @@ try
         var requestLine = request.Split("\r\n").FirstOrDefault();
         var requestLineParts = requestLine?.Split(" ");
         var httpMethod = requestLineParts?.FirstOrDefault();
-        var requestTarget = requestLineParts?.Skip(1).FirstOrDefault();
+        var requestTarget = requestLineParts?.Skip(1)
+            .FirstOrDefault()?
+            .Split("/", StringSplitOptions.RemoveEmptyEntries);
 
         if (string.IsNullOrWhiteSpace(requestLine) || string.IsNullOrWhiteSpace(httpMethod)
-            || string.IsNullOrWhiteSpace(requestTarget))
+            || httpMethod != "GET" || requestTarget is null)
         {
             var badRequest = System.Text.Encoding.ASCII.GetBytes("HTTP/1.1 400 Bad Request\r\n\r\n");
             stream.Write(badRequest, 0, badRequest.Length);
             continue;
         }
 
-        var message = requestTarget switch
+        var message = requestTarget.FirstOrDefault() switch
         {
-            "/" => "HTTP/1.1 200 OK\r\n\r\n",
+            null => "HTTP/1.1 200 OK\r\n\r\n",
+            "echo" => $"HTTP/1.1 200 OK\r\n" +
+                $"Content-Type: text/plain\r\n" +
+                $"Content-Length: {string.Join("/", requestTarget.Skip(1)).Length}\r\n\r\n" +
+                string.Join("/", requestTarget.Skip(1)),
             _ => "HTTP/1.1 404 Not Found\r\n\r\n"
         };
         var messageBytes = System.Text.Encoding.ASCII.GetBytes(message);
